45855f347f6723f4326de44f5b4ab847f9f68f50
STABISH-PC-PK\Tabish <s.tabish@epicgames.quixel.com>
2022-06-15 17:04:51 +0500

    no message

--- CopyAndPush.bat
+++ CopyAndPush.bat
@@ -1,4 +1,4 @@
-xcopy "%GITHUB_MIXER_UE_PROJECT_PATH%\%GITHUB_PR_NUMBER%\MixIntegrity\Difference" "www\%GITHUB_PR_NUMBER%" /E /H /C /I
+xcopy "%GITHUB_MIXER_UE_PROJECT_PATH%\%GITHUB_PR_NUMBER%\TestSuite\MixIntegrity\Difference" "www\%GITHUB_PR_NUMBER%" /E /H /C /I
 
 git add --all
 git diff-index --quiet HEAD || git commit -m "Scheduled commit"
